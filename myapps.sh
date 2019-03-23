b8d747abf0ad426289c7e9e4f0ace57f4e3f012b
Kevin Kivi <nake89@gmail.com>
2019-03-23 11:19:44 +0200

    Create myapps.sh

---
+++ myapps.sh
@@ -0,0 +1,10 @@
+# I'm still running ubuntu, so you're mileage might vary. This is really for future reference and not for running, imo
+
+# Install i3-gaps: https://www.github.com/Airblader/i3
+# I had to use this tutorial https://gist.github.com/dabroder/813a941218bdb164fb4c178d464d5c23
+
+# You can install normal i3 if you want.
+
+apt install i3status suckless-tools rxvt-unicode
+
+# Install this to be a be able to change font size on the fly: https://github.com/majutsushi/urxvt-font-size
